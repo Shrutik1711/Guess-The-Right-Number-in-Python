aa40de111c6e30ba637e5cad93d9e957673e2fe4
Shrutik Nandeshwar <123307004+Shrutik1711@users.noreply.github.com>
2025-07-18 12:50:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import random 
+n = random.randint(1,100)
+a = -1
+Guess = 1
+while (a!=n):
+    Guess+=1
+    a = int(input("Guess a number :-- "))
+    if (a<n):
+        print("Guess a higher number ")
+    
+    elif(a>n):
+        print("Guess a lower number ")
+        
+print(F"you guess the number {n} in {Guess} attempt")        
+        
